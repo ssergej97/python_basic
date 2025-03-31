74d1d93e22da98d75f90daee52c0c916d9508959
ssergej97 <sergejsubota1997@gmail.com>
2025-03-31 17:02:50 +0300

    lesson_2_edited

--- lesson_2/lesson_2_2.py
+++ lesson_2/lesson_2_2.py
@@ -6,6 +6,6 @@ number_3 = (user_input // 100) % 10
 number_4 = (user_input // 10) % 10
 number_5 = user_input % 10
 
-number_reverse = str(number_5) + str(number_4) + str(number_3) + str(number_2) + str(number_1)
+number_reverse = number_5 * 10000 + number_4 * 1000 + number_3 * 100 + number_2 * 10 + number_1
 
 print(number_reverse)
