68092ab6f3bfc3c29191445fcb26fdc5dc1e46e5
ssergej97 <sergejsubota1997@gmail.com>
2025-04-16 17:40:28 +0300

    lesson_5

--- lesson_5/lesson_5_1.py
+++ lesson_5/lesson_5_1.py
@@ -15,13 +15,24 @@ for i in user_input:
     elif i in string.punctuation and i != "_":
         check_is_upper = True
 
+y = []
+z = []
+for x in user_input:
+    if x == "_":
+        y.append(x)
+        space_count = y.count("_")
+    elif x != "_":
+        z.append(x)
+
 if user_input[0].isdigit():
     print("False")
 elif user_input in keyword.kwlist:
     print("False")
-elif user_input.count("_") > 1:
-    print("False")
 elif check_is_upper:
+    print("False")
+elif space_count > 1 and len(z) > 0:
+    print("True")
+elif space_count > 1:
     print("False")
 else:
     print("True")
