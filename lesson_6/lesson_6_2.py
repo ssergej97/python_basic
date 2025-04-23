b7e93b00d826beb093755d63094edba96ab1c3f9
ssergej97 <sergejsubota1997@gmail.com>
2025-04-23 17:46:31 +0300

    lesson_6

--- lesson_6/lesson_6_2.py
+++ lesson_6/lesson_6_2.py
@@ -1,4 +1,4 @@
-user_input_num = 7948799
+user_input_num = int(input("Введіть число для конвертації в дату від 0 до 8640000, наприклад 224930 "))
 
 user_input_days = user_input_num // (24 * 60 * 60)
 
@@ -18,4 +18,11 @@ user_input_minutes_in_sec = 60 * user_input_minutes
 
 user_input_seconds = user_input_minutes_in_sec_rest - user_input_minutes_in_sec
 
-print(f'{user_input_days} днів, {str(user_input_hours).zfill(2)}:{str(user_input_minutes).zfill(2)}:{str(user_input_seconds).zfill(2)}')
+if user_input_days % 10 == 1 and user_input_days % 100 != 11:
+    day_text = "день"
+elif 2 <= user_input_days % 10 <= 4 and (user_input_days % 100 < 10 or user_input_days % 100 >= 20):
+    day_text = "дні"
+else:
+    day_text = "днів"
+
+print(f'{user_input_days} {day_text}, {str(user_input_hours).zfill(2)}:{str(user_input_minutes).zfill(2)}:{str(user_input_seconds).zfill(2)}')
