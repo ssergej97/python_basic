e907bbecf41cf2ec7e5d839595761230ec2b87f4
ssergej97 <sergejsubota1997@gmail.com>
2025-04-25 11:08:42 +0300

    lesson_8

--- lesson_8/lesson_8_1.py
+++ lesson_8/lesson_8_1.py
@@ -1,11 +1,9 @@
 def add_one(some_list: list) -> list:
-    for index, value in enumerate(some_list):
-        some_list[index] = str(value)
+    some_list = list(map(str, some_list))
     num_string = int(''.join(some_list))
     num_string += 1
     new_lst = list(str(num_string))
-    for index, value in enumerate(new_lst):
-        new_lst[index] = int(value)
+    new_lst = list(map(int, new_lst))
     return new_lst
 assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
 assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
