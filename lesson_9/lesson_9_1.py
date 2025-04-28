d12aed2b9bfb0d48cad44ec2de3010eac59cfdb0
ssergej97 <sergejsubota1997@gmail.com>
2025-04-28 17:53:21 +0300

    lesson_9

---
+++ lesson_9/lesson_9_1.py
@@ -0,0 +1,18 @@
+def popular_words (text, words):
+    text_lower = text.lower()
+    text_lower_split = text_lower.split()
+    numbers_of_words = {word: 0 for word in words}
+
+    for i in text_lower_split:
+        if i in words:
+            numbers_of_words[i] += 1
+
+    return numbers_of_words
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
+print('OK')
+
+
+
+
+
+
