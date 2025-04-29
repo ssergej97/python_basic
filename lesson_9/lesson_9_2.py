648367ac29eccd467ac2a1098d527d6c8a083e5d
ssergej97 <sergejsubota1997@gmail.com>
2025-04-29 16:54:35 +0300

    lesson_9

--- lesson_9/lesson_9_2.py
+++ lesson_9/lesson_9_2.py
@@ -1,22 +1,13 @@
 def difference(*args) -> int | float:
-    list_of_numbers = []
+    if args:
+        max_of_numbers = max(args)
+        min_of_numbers = min(args)
 
-    for number in args:
-        list_of_numbers.append(number)
-
-    if len(list_of_numbers) == 0:
-        return 0
-
-    max_of_numbers = max(list_of_numbers)
-    min_of_numbers = min(list_of_numbers)
-
-    difference_of_numbers = max_of_numbers - min_of_numbers
-
-    if isinstance(difference_of_numbers, float):
-        difference_of_numbers = round(difference_of_numbers, 2)
+        difference_of_numbers = round(max_of_numbers - min_of_numbers, 2)
         return difference_of_numbers
     else:
-        return difference_of_numbers
+        return 0
+
 assert difference(1, 2, 3) == 2, 'Test1'
 assert difference(5, -5) == 10, 'Test2'
 assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
